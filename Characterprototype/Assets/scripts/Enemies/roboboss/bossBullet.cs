e8785941b82dd7cb3d524655ef70c7e4561d5e3c
Mark Thibodeaux <mthib34@lsu.edu>
2019-03-20 21:54:17 -0500

    boss change

--- Characterprototype/Assets/scripts/Enemies/roboboss/bossBullet.cs
+++ Characterprototype/Assets/scripts/Enemies/roboboss/bossBullet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class seedshoot : MonoBehaviour
+public class bossBullet : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject anima;
@@ -17,17 +17,17 @@ public class seedshoot : MonoBehaviour
     {
         Destroy(this.gameObject, 10);
         rb = GetComponent<Rigidbody2D>();
-        velocity = rb.velocity;
+         rb.velocity = velocity;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (rb.velocity.y < velocity.y)
-            rb.velocity = velocity;
+        //if (rb.velocity.y < velocity.y)
+        //    rb.velocity = velocity;
 
-        transform.Rotate(0,0, 6f);
+        //transform.Rotate(0, 0, 6f);
     }
 
 
@@ -52,4 +52,3 @@ public class seedshoot : MonoBehaviour
 
     }
 }
-
