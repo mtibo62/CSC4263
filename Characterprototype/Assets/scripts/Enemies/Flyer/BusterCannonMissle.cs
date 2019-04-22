9342299a997fe0e4f13a77691fe617698b5e17b7
Jeremy Kelly <jeremykelly@a4-5e-60-ee-7a-15.wlan.lsu.edu>
2019-04-22 13:54:12 -0500

    Stuff_udog
    ,

---
+++ Characterprototype/Assets/scripts/Enemies/Flyer/BusterCannonMissle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusterCannonMissle : MonoBehaviour
+{
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector2(player.position.x, player.position.y);
+
+    }
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+        if (transform.position.x  == target.x && transform.position.y == target.y)
+        {
+            DestroyBusterCannonMissle();
+        }
+
+
+        void OnTriggerEnter2D(Collider2D other){
+            if (other.CompareTag("Player"))
+            {
+                DestroyBusterCannonMissle();
+            }
+
+        }
+    }
+    void DestroyBusterCannonMissle()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Characterprototype/Assets/scripts/Enemies/Flyer/Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Move : MonoBehaviour { 
+
+    public float speed;
+    public float distance;
+
+    private bool movingRight = true;
+
+    public Transform skyDetection;
+
+    private void Update() 
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D skyInfo = Physics2D.Raycast(skyDetection.position, Vector2.down, distance);
+        if(skyInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            } else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+
+        }
+    }
+}
